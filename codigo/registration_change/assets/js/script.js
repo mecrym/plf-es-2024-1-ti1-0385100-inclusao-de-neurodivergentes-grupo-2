c9d654282fc30e06682679e47a4b5a95a9c2eff0
bruna markowisk <bruninha.markowisk@hotmail.com>
2024-05-12 19:02:05 -0300

    feat: js applied js in text field & need to be applied text area

--- codigo/registration_change/assets/js/script.js
+++ codigo/registration_change/assets/js/script.js
@@ -1,30 +1,98 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    var checkbox = document.querySelector('input[type="checkbox"]');
-    var buttonDone = document.querySelector('#addBtn');
-    buttonDone.addEventListener('click', function(){
-        alert("Hello");
+    const checkbox = document.querySelector('#switch');
+    const checkboxDone = document.querySelector('#switchBtn2');
+    const buttonDone = document.querySelector('#addBtn');
+    const addBtn2 = document.querySelector('#addText');
+    const divBtn = addBtn2.parentNode;
+    const containerText = divBtn.parentNode;
+    const childDivAdd = addBtn2.childNodes;
+    const divsContainer = containerText.childNodes;
+    const divP = document.querySelector('.container-p');
+    var textP = divP.childNodes;
+    var onClick = false;
+
+
+    addBtn2.addEventListener('click', (event)=>{
+      if(onClick){
+        onClick = false;
+        containerText.style.height = '300px';
+        childDivAdd[1].src = "/assets/images/minus.svg";
+        textP[1].textContent = "Add your title";
+        textP[1].style.color = '#FFC700';
+       
+        
+      }
+      else{
+        onClick = true;
+        childDivAdd[1].src = "/assets/images/plus_icon.svg";
+        containerText.style.height = '20%';
+        textP[1].textContent = "Add your text";
+        textP[1].style.color = '#FFFF';
+      }
+     
+      
+      
+    }, false);
+    
+    buttonDone.addEventListener('click', ()=>{
+        alert("Done");
     },false);
+
+    checkboxDone.addEventListener('change', ()=> {
+      if(checkboxDone.checked){
+        console.log("checked 2° btn");
+      }
+    });
   
-    checkbox.addEventListener('change', function () {
+    checkbox.addEventListener('change', ()=> {
+      const sectionDone = document.querySelector('.completed-task');
       if (checkbox.checked) {
-        // do this
+        sectionDone.style.visibility = "hidden";
         console.log("Checked");
+        var section = document.createElement("section");
+        section.setAttribute("class", "container-date");
+        var text = document.createElement('p');
+        text.textContent = "End Time:";
+        section.appendChild(text);
+        var timeEnd = document.createElement('input');
+        let elementChild = document.querySelector('body > main > section.add-button');
+        timeEnd.setAttribute("type","time");
+        timeEnd.setAttribute("id","time");
+        timeEnd.setAttribute("min","12:00");
+        timeEnd.setAttribute("max", "18:00");
+        timeEnd.setAttribute("required", "");
+        section.appendChild(timeEnd);
+        var elementParent = elementChild.parentNode;
+        elementParent.insertBefore(section,elementChild);
+        
+
         var sectionStart = document.createElement("section");
         sectionStart.setAttribute("class", "container-date");
+        var text = document.createElement("p");
+        text.textContent= "Start Time";
+        sectionStart.appendChild(text);
         var timeStart = document.createElement('input');
-        var elementChild = document.querySelector('body > main > section.add-button');
+        elementChild = document.querySelector('body > main > section.container-date');
         timeStart.setAttribute("type","time");
-        timeStart.setAttribute("id","timeStart");
+        timeStart.setAttribute("id","time");
         timeStart.setAttribute("min","12:00");
         timeStart.setAttribute("max", "18:00");
         timeStart.setAttribute("required", "");
         sectionStart.appendChild(timeStart);
-        var elementParent = elementChild.parentNode;
+        elementParent = elementChild.parentNode;
         elementParent.insertBefore(sectionStart,elementChild);
-
+        text.style.marginTop = '-100px'; 
+        
       } else {
-        // do that
+        const sections = document.querySelectorAll('.container-date');
+        
+        if(sections){
+          sections.forEach(section => {
+            section.remove(); 
+          });
+        }
+        sectionDone.style.visibility = "visible";
         console.log("Not checked");
       }
     }); 
