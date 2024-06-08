d35ce3d6348fe66e3767cc7a23b7b7b72355ed1c
bruna markowisk <bruninha.markowisk@hotmail.com>
2024-06-08 15:10:13 -0300

    feat: add pexels api

--- codigo/post/public/assets/js/selectImage.js
+++ codigo/post/public/assets/js/selectImage.js
@@ -1,14 +1,42 @@
-export class SelectImage{
+//import { createClient } from '../../../node_modules/pexels';
+
+export class SelectImage {
+    
     constructor() {
-        this.urlBase = 'http://localhost:3000/users';
-      }
-    async getUsers(){
-        try{
-            const res = await fetch(this.urlBase);
-            return res.json();
-        }catch{
-            console.log("Error in fetch db from JSONServer");
-        }
-        
+        this.key = 'Lt9am0k6ijWEDjgVOyxWsfjNV06AAdZ91lhqOM1yh5IwNgxgpyaDENqu';
+        this.query = 'Gym';
     }
+    async getPhotos(){ 
+        fetch(`https://api.pexels.com/v1/search?query=${this.query}`,{
+            headers: {
+              Authorization: this.key
+            }
+          })
+             .then(resp => {
+               return resp.json()
+             })
+             .then(data => {
+               console.log(data)
+             })
+    }
+    async getPhoto(pos){
+        fetch(`https://api.pexels.com/v1/search?query=${this.query}`,{
+            headers: {
+              Authorization: this.key
+            }
+          })
+          .then(resp => resp.json()) // Converte a resposta para JSON
+          .then(data => {
+            if (data.photos && data.photos.length > 0) {
+              const photoUrl = data.photos[pos].src.original;
+              console.log("URL:", photoUrl);
+            } else {
+              console.log("This photo doesn't have a url");
+            }
+          })
+          .catch(error => {
+            console.error("Error to fetch pexels:", error);
+          });
+    }
+
 }
