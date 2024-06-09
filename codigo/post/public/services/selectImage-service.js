1f5f5b10bf7af41ea42eea4b019ffc83222c3da1
bruna markowisk <bruninha.markowisk@hotmail.com>
2024-06-09 10:15:05 -0300

    feat: add border when clicked in image & return url to class

--- codigo/post/public/services/selectImage-service.js
+++ codigo/post/public/services/selectImage-service.js
@@ -3,6 +3,7 @@ export class SelectImage {
     constructor() {
         this.key = 'Lt9am0k6ijWEDjgVOyxWsfjNV06AAdZ91lhqOM1yh5IwNgxgpyaDENqu';
         this.query = 'Gym';
+        this.url = '';
     }
     
     async getPhotos() { 
@@ -13,7 +14,6 @@ export class SelectImage {
                 }
             });
             const data = await response.json();
-            console.log(data);
         } catch (error) {
             console.error("Error to fetch pexels:", error);
         }
@@ -28,13 +28,17 @@ export class SelectImage {
             });
             const data = await response.json();
             if (data.photos && data.photos.length > 0) {
-               console.log("url: ",data.photos);
-                return data.photos[pos].src.original;
+               this.url = data.photos[pos].src.original;
+            //   console.log("url: ",this.url);
+                return this.url;
             } else {
                 console.log("This photo doesn't have a url");
             }
         } catch (error) {
             console.error("Error to fetch pexels:", error);
         }
+    }
+    getUrlPhoto(){
+       return this.url;
     }
 }
