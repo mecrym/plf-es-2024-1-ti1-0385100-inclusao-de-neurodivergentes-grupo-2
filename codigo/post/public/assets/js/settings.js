94a96e5efe441052d8a279c069ca1bb110fbe0f8
bruna markowisk <bruninha.markowisk@hotmail.com>
2024-06-26 17:04:45 -0300

    feat: add name, email and password fields with content and add img to toggle password view

--- codigo/post/public/assets/js/settings.js
+++ codigo/post/public/assets/js/settings.js
@@ -16,4 +16,23 @@ document.addEventListener('DOMContentLoaded', async function () {
     const userAccount = await getUser(ID);
     const img = document.querySelector('.profile-picture>img');
     img.setAttribute("src",userAccount.profilePhotoUrl);
+    const inputName = document.querySelector('.name');
+    inputName.setAttribute("value", userAccount.name);
+    const inputEmail = document.querySelector('.email');
+    inputEmail.setAttribute("value", userAccount.email);
+    const inputPassword = document.querySelector('.password');
+    inputPassword.setAttribute("value", userAccount.password);
+    const imgView = document.querySelector('.view>img');
+    console.log(imgView);
+    imgView.addEventListener('click', ()=>{
+        var passwordFieldType = inputPassword.getAttribute('type');
+        if (passwordFieldType === 'password') {
+            inputPassword.setAttribute('type', 'text');
+            imgView.setAttribute("src", "../../assets/images/unview.svg");
+        } else {
+            inputPassword.setAttribute('type', 'password');
+            imgView.setAttribute("src", "../../assets/images/view.svg");
+        }
+
+    });
 });
